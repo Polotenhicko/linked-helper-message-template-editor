cabaf963a1b598f221343c1182ad634c96365d2d
Polo <artem-gribanof@yandex.ru>
2023-07-31 23:52:54 +0300

    script for create components pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import './App.css';
+import { useState } from 'react';
+import { MessageEditor } from './components/MessageEditor';
+import { Button } from './controls/Button';
 
 export function App() {
+  const [isOpenMessageEditor, setIsOpenMessageEditor] = useState(false);
+
+  const toggleMessageEditor = () => {
+    setIsOpenMessageEditor(!isOpenMessageEditor);
+  };
+
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Button onClick={toggleMessageEditor}>Message Editor</Button>
+      {isOpenMessageEditor && <MessageEditor />}
+    </>
   );
 }
-
