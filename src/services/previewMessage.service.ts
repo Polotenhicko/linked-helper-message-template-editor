d52d18f238cd337700dc88364f12512ee6a69758
Polo <artem-gribanof@yandex.ru>
2023-08-12 11:59:23 +0300

    add temp func

--- src/services/previewMessage.service.ts
+++ src/services/previewMessage.service.ts
@@ -92,3 +92,61 @@ class PreviewMessageService extends ObserverService {
 const previewMessageService = new PreviewMessageService();
 
 export default previewMessageService;
+
+function generateMessage(template: ITemplate, values: IVarNamesObj, arrVarNames: TArrVarNames): string {
+  const getReplacedVariables = (text: string): string => {
+    if (!text) return '';
+
+    for (const [variable, value] of Object.entries(values)) {
+      if (arrVarNames.includes(variable)) {
+        text = text.split(`{${variable}}`).join(value);
+      }
+    }
+
+    return text;
+  };
+
+  const getCompiledIfBlock = (ifBlock: IConditionalOperatorObj): boolean => {
+    const firstText = getReplacedVariables(ifBlock.firstText);
+    const secondText = getReplacedVariables(ifBlock.secondText);
+
+    if (firstText || secondText) return true;
+
+    const stringsConditionalBlocks = getCompiledConditionalBlocks(ifBlock.conditionalBlocks);
+
+    const hasConditionalResultText = !!stringsConditionalBlocks.length;
+
+    return hasConditionalResultText;
+  };
+
+  const getCompiledThenElseBlock = (thenElseBlock: IConditionalOperatorObj): string => {
+    const firstText = getReplacedVariables(thenElseBlock.firstText);
+    const secondText = getReplacedVariables(thenElseBlock.secondText);
+
+    let stringConditionalBlocks = getCompiledConditionalBlocks(thenElseBlock.conditionalBlocks);
+
+    return firstText + stringConditionalBlocks + secondText;
+  };
+
+  const getCompiledConditionalBlocks = (conditionalBlocks: IConditionalBlock[]): string => {
+    const result: string[] = [];
+
+    for (const block of conditionalBlocks) {
+      const isTrueConditional = getCompiledIfBlock(block.if);
+
+      if (isTrueConditional) {
+        result.push(getCompiledThenElseBlock(block.then));
+      } else {
+        result.push(getCompiledThenElseBlock(block.else));
+      }
+    }
+
+    return result.filter((resultBlock) => resultBlock).join('');
+  };
+
+  const stringConditionalBlocks = getCompiledConditionalBlocks(template.conditionalBlocks);
+  const startMessage = getReplacedVariables(template.startMessage);
+  const finalMessage = getReplacedVariables(template.finalMessage);
+
+  return startMessage + stringConditionalBlocks + finalMessage;
+}
